bdd2f37973fc7d07afd0ea51e69473ec3f8a8e02
yorifuji <yorifuji@gmail.com>
2021-03-17 09:26:18 +0900

    Add project

---
+++ TextField-letters-and-digits/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TextField-letters-and-digits
+//
+//  Created by yorifuji on 2021/03/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TextField-letters-and-digits/TextField_letters_and_digitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextField_letters_and_digitsApp.swift
+//  TextField-letters-and-digits
+//
+//  Created by yorifuji on 2021/03/17.
+//
+
+import SwiftUI
+
+@main
+struct TextField_letters_and_digitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
