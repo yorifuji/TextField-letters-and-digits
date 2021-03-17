c8695fd51cf7a72f61ba72863bd144c75d97d2f5
yorifuji <yorifuji@gmail.com>
2021-03-17 09:41:35 +0900

    TextField

--- TextField-letters-and-digits/ContentView.swift
+++ TextField-letters-and-digits/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var username = ""
     var body: some View {
-        Text("Hello, world!")
+        TextField("[A-Za-z0-9]", text: $username)
+            .autocapitalization(.none)
+            .keyboardType(.asciiCapable)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
             .padding()
+            .onChange(of: username, perform: filter )
+    }
+
+    func filter(value: String) {
+        let sets = CharacterSet.lowercaseLetters
+            .union(CharacterSet.uppercaseLetters)
+            .union(CharacterSet.decimalDigits)
+        username = String(value
+                            .unicodeScalars
+                            .filter(sets.contains)
+                            .map(Character.init))
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.sizeThatFits)
     }
 }
